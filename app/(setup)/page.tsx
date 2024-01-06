6074f60e4cbbd5765be9b5fe2e1b2f477505220d
sagarkashyap989 <saga11kashyap@mail.com>
2024-01-06 12:13:29 +0530

    add initial modal , with server name input

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import InitialModal from "@/components/modal/initial-modal";
 const  SetupPage = async() => {
 
     const profile =  await initialProfile()
@@ -9,7 +10,7 @@ const  SetupPage = async() => {
         where:{
             members:{
                 some:{
-                    profileId: profile.id 
+                    profileId: profile?.id 
                 }
             }
         }
@@ -19,7 +20,7 @@ const  SetupPage = async() => {
         return redirect(`/servers/${server.id}`)
     }
 
-    return ( <div>create a server</div> );
+    return <InitialModal />;
 }
  
 export default SetupPage;
